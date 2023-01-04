ef396329fb0b3528e9be842cf3bfecdf50911cb9
isaac <isaaccastrocisneros@gmail.com>
2023-01-04 15:24:56 -0500

    leeel

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,24 +1,24 @@
-import axios from 'axios'
 import React from 'react'
 import Results from './components/Results/Results';
-import postRequest from './components/Results/utils/postRequest';
 
 interface props
 {
   searchParams:searchParams
 }
+
 interface searchParams
 {
   games?:string
 }
 
-export default async function page({searchParams}:props) 
+export default function page(/* {searchParams}:props */) 
 {
-  const{games}=searchParams
+/*   const{games}=searchParams
+  console.log(searchParams) */
 
   return (
       <main className="custom-container">
-        <Results param={games||''} />
+       {/*  <Results param={games||''} /> */}
       </main>
   );
 }
